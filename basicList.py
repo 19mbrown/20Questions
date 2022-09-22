76bba11f7338d77bc89b23485aef927e2250b367
19mbrown <114150091+19mbrown@users.noreply.github.com>
2022-09-22 12:25:29 +0100

    Add code

---
+++ basicList.py
@@ -0,0 +1,32 @@
+import os
+
+yesOrNo = str()
+listOfNames = list()
+
+while yesOrNo != "y":
+    name = input("Enter a name: ")
+    listOfNames.append(name)
+    yesOrNo = input("Do you want to carry on? (y/n) ")
+listOfNames = listOfNames.lower()
+
+printList = input("Do you want to print the list of names normally or reversed? (n/r) ")
+
+if printList == "r":
+    print(listOfNames.revers())
+else if printList == "n":
+    print(listOfNames)
+
+if input("Do you want to print out a specific number? (y/n) ") == "y":
+    print(listOfNames[int(input(f"Which number do you want to print out? {len(listOfNames)} ") ) + 1])
+
+if input("Do you want to print out the list sliced? (y/n) ") == "y":
+    sliceStart = int(input("Where do you want to start at? ") + 1)
+    sliceEnd = int(input("Where do you want to end at? ") + 1)
+    print(listOfNames[sliceStart:sliceEnd])
+
+if input("Do you want to remove any items? (y/n) ") == "y":
+    listOfNames.remove(input("What item do you want to remove?"))
+
+if input("Do you want to save this to a file? ") =="y":
+    with open('bak.bak', 'w') as f:
+        f.write(listOfNames)
---
+++ passReset.py
@@ -0,0 +1,34 @@
+#########################
+#### NOT WORKING YET ####
+#########################
+
+
+import string
+
+
+def hasUppers(a):
+    for i in a:
+        if i in string.ascii_uppercase:
+            return True
+            break
+        else:
+            return False
+def hasLowers(a):
+    for i in a:
+        if i in string.ascii_lowercase:
+            return True
+            break
+        else:
+            return False 
+global password
+password = str()
+
+def password():
+    while len(password) < 8 or not (hasLowers(password) and hasUppers(password)):
+        password = input("What's your password? ")
+        if not len(password) < 8 or not (hasLowers(password) and hasUppers(password)):
+            password2 = input("Retype the password: ")
+if password == password2:
+    print("Correct Password!!")
+else:
+    password()
