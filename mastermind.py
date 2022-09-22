02fb6ef39cf29a0fd8b2df4044140522365817bf
19mbrown <114150091+19mbrown@users.noreply.github.com>
2022-09-22 11:17:52 +0100

    Create mastermind.py

---
+++ mastermind.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+### MASTER MIND
+
+from random import randrange
+
+
+number = randrange(0,9999)
+print(number)
+userIn = str()
+
+
+while len(userIn) != 4:
+    userIn = input("Pick a 4 digit number: ")
+
+while int(userIn) != number:
+    print("Wrong guess! ", end='')
+    userIn = input("Pick aother 4 didgit number: ")
+print("You've got the right number! ")
