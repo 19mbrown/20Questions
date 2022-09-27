f267b8c3e79bf947704880d98f18bb65ea93493b
19mbrown <114150091+19mbrown@users.noreply.github.com>
2022-09-27 21:06:46 +0000

    Update RPGcreator.py

--- RPGcreator.py
+++ RPGcreator.py
@@ -5,13 +5,16 @@ import random
 ## NOT WORKING YET ##
 #####################
 
-global genders, charClasses, strength, powers, extraAbilities
+global genders, charClasses, strength, powers
 
-genders = ["Female"", " "Male"]
+genders = ["Female", "Male"]
 charClasses = ["Normal", " Fire", " Water", " Grass", " Electric", " Ice", " Fighting", " Poison", " Ground", " Flying", " Psychic", " Bug", " Rock", "Ghost", "Dragon"]
 powers = ["Invisibility", "Shapshitfting", "Magnetism", "Night Vision", "Danger Sense"]
 
 class character:
     gender = random.choice(genders)
     charClass = random.choice(charClasses)
-    power = 
+    power = random.choice(powers)
+    strength = random.randrange(1,100)
+
+print(f"STRENGTH: {character.strength}\nGender: {character.gender}\nPowers: {character.power}\nClass: {character.charClass}")
