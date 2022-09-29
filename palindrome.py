0759dfc472a4c133d484e5d69a85e9aa6b1280d9
19mbrown <114150091+19mbrown@users.noreply.github.com>
2022-09-29 11:12:47 +0100

    Create palimdrome.py

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+def palindrome(word):
+    wordList = list(word)
+    wordBack = wordList[::-1]
+    for i in range(len(wordList)):
+        if wordBack[i] == wordList[i]:
+            return True
+        else:
+            return False
+            break
