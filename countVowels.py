aba18380305d8584dd91783415b9239a6546ce2b
19mbrown <114150091+19mbrown@users.noreply.github.com>
2022-10-04 12:19:52 +0100

    Update countVowels.py

--- countVowels.py
+++ countVowels.py
@@ -2,6 +2,6 @@ def countVowels(string):
     vowels = ["a", "e", "i", "o", "u"]
     numberOfVowels = dict()
     for i in vowels:
-        numberOfVowels[i] = string.count(i)
+        numberOfVowels[i] = string.lower().count(i)
     numberOfVowels["TOTAL"] = sum(numberOfVowels.values())
     return numberOfVowels
