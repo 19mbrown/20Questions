6905a2cc33cfd7a6e9f9e16bcf5043e2a617b12b
19mbrown <114150091+19mbrown@users.noreply.github.com>
2022-10-04 18:17:31 +0000

    Update RPGcreator.py

--- RPGcreator.py
+++ RPGcreator.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 import random
 
-#####################
-## NOT WORKING YET ##
-#####################
-
 global genders, charClasses, strength, powers
 
 genders = ["Female", "Male"]
-charClasses = ["Normal", " Fire", " Water", " Grass", " Electric", " Ice", " Fighting", " Poison", " Ground", " Flying", " Psychic", " Bug", " Rock", "Ghost", "Dragon"]
-powers = ["Invisibility", "Shapshitfting", "Magnetism", "Night Vision", "Danger Sense"]
+powers = ["Fire", "Water", "Electric", "Ice", "Fighting", "Poison", "Flying", "Psychic", "Ghost", "Dragon", "Invisibility", "Shapshitfting", "Magnetism", "Night Vision", "Danger Sense"]
+charClasses = ["The Fighter", "The Rogue", "The Magician", "The Rangers", "The Clerics", "Rare Character"]
 
 class character:
     gender = random.choice(genders)
-    charClass = random.choice(charClasses)
     power = random.choice(powers)
+    charClass = random.choice(charClasses)
     strength = random.randrange(1,100)
 
-print(f"STRENGTH: {character.strength}\nGender: {character.gender}\nPowers: {character.power}\nClass: {character.charClass}")
+print(f"Strength: {character.strength}\nGender: {character.gender}\nPowers: {character.power}\nClass: {character.charClass}")
