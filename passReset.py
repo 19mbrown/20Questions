045d228314f3c447901acba2fd4fe9ca6051a595
19mbrown <114150091+19mbrown@users.noreply.github.com>
2022-10-04 18:11:47 +0000

    passReset rewrite.

--- passReset.py
+++ passReset.py
@@ -1,32 +1,58 @@
-#########################
-#### NOT WORKING YET ####
-#########################
-
-
-import string
-
+import string as s
+from zxcvbn import zxcvbn 
 
 def hasUppers(a):
+    x = bool()
     for i in a:
-        if i in string.ascii_uppercase:
-            return True
-            break
+        if i in s.ascii_uppercase:
+            x = True
         else:
-            return False
-def hasLowers(a):
-    for i in a:
-        if i in string.ascii_lowercase:
-            return True
+            x = False
+        if x:
             break
-        else:
-            return False 
-password = str()
+    return x
 
-while len(password) < 8 or not (hasLowers(password) and hasUppers(password)):
-    password = input("What's your password? ")
-    if not len(password) < 8 or not (hasLowers(password) and hasUppers(password)):
-        password2 = input("Retype the password: ")
-if password == password2:
-    print("Correct Password!!")
-else:
-    password()
+def hasLowers(a):
+    x = bool()
+    for i in a:
+        if i in s.ascii_lowercase:
+            x = True
+        else:
+            x = False
+        if x:
+            break
+    return x
+
+def passwords():
+    p1 = input("What's your password? ")
+    p2 = input("Retype the password? ")
+    if p1 == p2:
+        return [True, p1]
+    else:
+        return [False, p1]
+
+def passReset():
+    global password
+    x = 0
+
+    passFunc = passwords()
+    password = passFunc[1]
+    while not (passFunc[0]):
+        print("Passwords dont match! ")
+        password = passwords()[1]
+
+    ## check if password has upper and lower case 
+    if not (hasUppers(password) and hasLowers(password)):
+        print("Must have uppercase and lowercase! ")
+        print("PERMISSION DENIED")
+        exit(1)
+
+    if len(password) <= 8:
+        print("Must be at least 8 characters")
+        print("PERMISSION DENIED")
+        exit(1)
+    
+    print("Password entered succesfully! ")
+    print(f"Your password is given a score of {int(zxcvbn(password)['score']) * 25}")
+
+passReset()
